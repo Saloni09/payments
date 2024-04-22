98b767ad0b9ae4ea66d72a90cfd74dd97fab7416
Saloni Verma <salverma@WKMZT72733F9.global.publicisgroupe.net>
2024-04-22 16:48:17 +0530

    db connection added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaymentsController } from './payments/payments.controller';
 import { PaymentsModule } from './payments/payments.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      host:'localhost',
+      port:5432,
+      username:'postgres',
+      password:'postgres',
+      database: 'payment-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    PaymentsModule],
   controllers: [AppController, PaymentsController],
   providers: [AppService],
 })
